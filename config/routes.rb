77e7e98933ba3ea47fe7b54b056506252808b94a
Mikhail Sotnikov <thehofch@gmail.com>
2018-07-25 14:38:59 +0300

    remove format

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :tasks
-  devise_for :users
+  scope format: false do
+    resources :tasks
+    devise_for :users
+  end
   root to: 'tasks#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
