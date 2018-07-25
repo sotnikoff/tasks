40c9dbac025dcbdbc7e1ae4cf702dba0397a5b4a
Mikhail Sotnikov <thehofch@gmail.com>
2018-07-25 12:39:11 +0300

    add enum status to task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,10 @@
 class Task < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :assigned_to, class_name: 'User'
+
+  enum status: {
+    status_new: 'new',
+    status_started: 'started',
+    status_finished: 'finished'
+  }
 end
