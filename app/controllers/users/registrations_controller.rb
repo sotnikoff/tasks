6800f177b4ecc70f2353d33b3506d1166c0a61d7
Mikhail Sotnikov <thehofch@gmail.com>
2018-07-26 13:09:46 +0300

    add validation

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_account_update_params, only: [:update]
 
   protected
-  
+
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
   end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,8 @@ class Task < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :assigned_to, class_name: 'User'
 
+  validates :title, presence: true
+
   enum status: {
     status_new: 'new',
     status_started: 'started',
