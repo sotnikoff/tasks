c5a84430e0cc73e1947cd90b7d7d3750d3573f37
Mikhail Sotnikov <thehofch@gmail.com>
2018-07-26 09:51:46 +0300

    add last name and first name to devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   scope format: false do
     resources :tasks
-    devise_for :users
+    devise_for :users, controllers: {
+      registrations: 'users/registrations'
+    }
   end
   root to: 'tasks#index'
 end
